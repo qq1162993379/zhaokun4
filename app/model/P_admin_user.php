a4be443921a58799d56f9bfc0f9d496f62f50b34
qq1162993379 <1162993379@qq.com>
2022-07-29 18:00:43 +0800

    11

--- app/model/P_admin_user.php
+++ app/model/P_admin_user.php
@@ -7,7 +7,7 @@ use think\Model;
 // Counters 定义数据库model
 class P_admin_user extends Model
 {
-    protected $table = 'P_admin_user';
+    protected $table = 'p_admin_user';
     public $id;
     // public $opid;
     public $username;
--- app/model/P_qiantai_user.php
+++ app/model/P_qiantai_user.php
@@ -7,7 +7,7 @@ use think\Model;
 // Counters 定义数据库model
 class P_qiantai_user extends Model
 {
-    protected $table = 'P_qiantai_user';
+    protected $table = 'p_qiantai_user';
     public $id;
     // public $opid;
     public $username;
