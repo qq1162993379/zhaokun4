3f166c3819b3a4025985289443a9b87a0badcf34
qq1162993379 <1162993379@qq.com>
2022-08-09 20:12:22 +0800

    222

--- config/database.php
+++ config/database.php
@@ -20,55 +20,19 @@ return [
 
     // 数据库连接配置信息
     'connections' => [
-        // 'mysql' => [
-        //     // 数据库类型
-        //     'type' => 'mysql',
-        //     // 服务器地址
-        //     'hostname' => preg_split('/:/',getenv('MYSQL_ADDRESS'))[0],
-        //     // 服务器端口
-        //     'hostport' =>  preg_split('/:/',getenv('MYSQL_ADDRESS'))[1],
-        //     // 用户名
-        //     'username' => getenv('MYSQL_USERNAME'),
-        //     // 密码
-        //     'password' => getenv('MYSQL_PASSWORD'),
-        //     // 数据库名
-        //     'database' => (getenv('MYSQL_DATABASE') == null) ? 'thinkphp_demo' : getenv('MYSQL_DATABASE'),
-        //     // 数据库连接参数
-        //     'params' => [],
-        //     // 数据库编码默认采用utf8
-        //     'charset' => 'utf8',
-        //     // 数据库表前缀
-        //     'prefix' => '',
-        //     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-        //     'deploy' => 0,
-        //     // 数据库读写是否分离 主从式有效
-        //     'rw_separate' => false,
-        //     // 读写分离后 主服务器数量
-        //     'master_num' => 1,
-        //     // 指定从服务器序号
-        //     'slave_no' => '',
-        //     // 是否严格检查字段是否存在
-        //     'fields_strict' => true,
-        //     // 是否需要断线重连
-        //     'break_reconnect' => false,
-        //     // 监听SQL
-        //     'trigger_sql' => env('app_debug', true),
-        //     // 开启字段缓存
-        //     'fields_cache' => false,
-        // ],
         'mysql' => [
             // 数据库类型
             'type' => 'mysql',
             // 服务器地址
-            'hostname' => '127.0.0.1',
+            'hostname' => preg_split('/:/',getenv('MYSQL_ADDRESS'))[0],
             // 服务器端口
-            'hostport' =>  3306,
+            'hostport' =>  preg_split('/:/',getenv('MYSQL_ADDRESS'))[1],
             // 用户名
-            'username' => 'root',
+            'username' => getenv('MYSQL_USERNAME'),
             // 密码
-            'password' => '',
+            'password' => getenv('MYSQL_PASSWORD'),
             // 数据库名
-            'database' => 'zhaokun',
+            'database' => (getenv('MYSQL_DATABASE') == null) ? 'thinkphp_demo' : getenv('MYSQL_DATABASE'),
             // 数据库连接参数
             'params' => [],
             // 数据库编码默认采用utf8
@@ -92,6 +56,42 @@ return [
             // 开启字段缓存
             'fields_cache' => false,
         ],
+        // 'mysql' => [
+        //     // 数据库类型
+        //     'type' => 'mysql',
+        //     // 服务器地址
+        //     'hostname' => '127.0.0.1',
+        //     // 服务器端口
+        //     'hostport' =>  3306,
+        //     // 用户名
+        //     'username' => 'root',
+        //     // 密码
+        //     'password' => '',
+        //     // 数据库名
+        //     'database' => 'zhaokun',
+        //     // 数据库连接参数
+        //     'params' => [],
+        //     // 数据库编码默认采用utf8
+        //     'charset' => 'utf8',
+        //     // 数据库表前缀
+        //     'prefix' => '',
+        //     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+        //     'deploy' => 0,
+        //     // 数据库读写是否分离 主从式有效
+        //     'rw_separate' => false,
+        //     // 读写分离后 主服务器数量
+        //     'master_num' => 1,
+        //     // 指定从服务器序号
+        //     'slave_no' => '',
+        //     // 是否严格检查字段是否存在
+        //     'fields_strict' => true,
+        //     // 是否需要断线重连
+        //     'break_reconnect' => false,
+        //     // 监听SQL
+        //     'trigger_sql' => env('app_debug', true),
+        //     // 开启字段缓存
+        //     'fields_cache' => false,
+        // ],
 
         // 更多的数据库配置信息
     ],
